7a16faa7a3e424b1ce3eb70856a80169d8d3bd76
RaedAlsayeh <raed.sayeh@hotmail.com>
2020-11-25 00:54:38 +0200

    --- Auto-Git Commit ---

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,41 @@
 require('./bootstrap');
+/*
+|----------------------------------------------------------------
+| Vue 3
+|----------------------------------------------------------------
+*/
+
+import { createApp, h } from 'vue'
+import { App, plugin } from '@inertiajs/inertia-vue3'
+
+const el = document.getElementById('app')
+
+createApp({
+  render: () => h(App, {
+    initialPage: JSON.parse(el.dataset.page),
+    resolveComponent: name => require(`./Pages/${name}`).default,
+  })
+}).use(plugin).mount(el)
+
+
+/*
+|----------------------------------------------------------------
+| Vue 2
+|----------------------------------------------------------------
+*/
+
+import { App, plugin } from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+
+Vue.use(plugin)
+
+const el = document.getElementById('app')
+
+new Vue({
+  render: h => h(App, {
+    props: {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: name => require(`./Pages/${name}`).default,
+    },
+  }),
+}).$mount(el)
